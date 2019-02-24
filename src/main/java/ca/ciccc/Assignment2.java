d1ef322fdc1371d43408645593c8dccc3bda94fb
Derrick Park <derrick8689@gmail.com>
2019-02-24 07:52:01 -0800

    Initial Commit

---
+++ src/main/java/ca/ciccc/Assignment2.java
@@ -0,0 +1,92 @@
+package ca.ciccc;
+
+/**
+ * This is the starter file for the Assignment 2
+ *
+ * @author Derrick Park
+ * @version 1.0
+ */
+public class Assignment2 {
+    /**
+     * Write nested for-loops to produce the following output from the given input n.
+     * @param n number to create a pattern.
+     * @return pattern
+     *
+     * Sample Input / Output
+     * numberLoops(3);
+     *
+     * Sample Output
+     * ..1
+     * .22
+     * 333
+     *
+     */
+    public String numberLoops(int n) {
+
+        return null;
+    }
+
+    /**
+     * Given an array of integers, return true if the number of 1s is greater than the number of 7s
+     * @param nums array of ints
+     * @return true if the number of 1s is greater than the number of 7s
+     */
+    public boolean countNumbers(int[] nums) {
+
+        return false;
+    }
+
+    /**
+     * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13
+     * is very unlucky, so it does not count the number that comes immediately after a 13, also 13 itself
+     * does not count.
+     * @param nums array of numbers
+     * @return the sum except for 13
+     *
+     * Sample Input / Output
+     * sumExcept13({1, 2, 2, 1}) -> 6
+     * sumExcept13({1, 2, 2, 1, 13} -> 6
+     * sumExcept13({1, 2, 13, 2, 1, 13} -> 4
+     */
+    public int sumExcept13(int[] nums) {
+
+        return 0;
+    }
+
+    /**
+     * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} return {2, 5, 3, 6}.
+     * You may modify and return the given array, or return a new array.
+     * @param nums array of ints
+     * @return shifted array
+     *
+     * Sample Input / Output
+     * shiftArray({6, 2, 5, 3}) -> {2, 4, 3, 6}
+     * shiftArray({2, 3}) -> {3, 2}
+     * shiftArray({7}) -> {7}
+     */
+    public int[] shiftArray(int[] nums) {
+
+        return null;
+    }
+
+    /**
+     * Return true if the array contains three increasing adjacent numbers, otherwise false.
+     * @param nums array of ints.
+     * @return true if the array contains three increasing adjacent numbers, otherwise false.
+     */
+    public boolean tripleIncreasingOrder(int[] nums) {
+
+        return false;
+    }
+
+    /**
+     * Return true if the array contains the same number of odds and even numbers, otherwise false.
+     * @param nums array of ints.
+     * @return true if the array contains the same number of odds and even numbers, otherwise false.
+     */
+    public boolean evenOrOdd(int[] nums){
+
+        return false;
+    }
+}
+
---
+++ src/test/java/Grader.java
@@ -0,0 +1,72 @@
+import ca.ciccc.Assignment2;
+import org.junit.*;
+
+public class Grader {
+
+    private static Assignment2 assignment2;
+
+    @BeforeClass
+    public static void setup() {
+        assignment2 = new Assignment2();
+    }
+
+    @Test
+    public void testNumberLoops() {
+        Assert.assertEquals("1", assignment2.numberLoops(1));
+        Assert.assertEquals("..1\n.22\n333", assignment2.numberLoops(3));
+        Assert.assertEquals("....1\n...22\n..333\n.4444\n55555", assignment2.numberLoops(5));
+    }
+
+    @Test
+    public void testCountNumbers() {
+        Assert.assertEquals(true, assignment2.countNumbers(new int[]{1, 7, 1}));
+        Assert.assertEquals(false, assignment2.countNumbers(new int[]{1, 7, 1, 7}));
+        Assert.assertEquals(false, assignment2.countNumbers(new int[]{1, 2, 2, 7, 1, 7, 7}));
+        Assert.assertEquals(false, assignment2.countNumbers(new int[]{}));
+        Assert.assertEquals(true, assignment2.countNumbers(new int[]{1, 7, 1, 7, 1, 6}));
+    }
+
+    @Test
+    public void testSumExcept13() {
+        Assert.assertEquals(6, assignment2.sumExcept13(new int[]{1, 2, 2, 1}));
+        Assert.assertEquals(0, assignment2.sumExcept13(new int[]{}));
+        Assert.assertEquals(6, assignment2.sumExcept13(new int[]{1, 2, 2, 1, 13}));
+        Assert.assertEquals(4, assignment2.sumExcept13(new int[]{1, 2, 13, 2, 1, 13}));
+        Assert.assertEquals(0, assignment2.sumExcept13(new int[]{13, 1, 13}));
+    }
+
+    @Test
+    public void testShiftArray() {
+        Assert.assertArrayEquals(new int[]{2, 5, 3, 6}, assignment2.shiftArray(new int[]{6, 2, 5, 3}));
+        Assert.assertArrayEquals(new int[]{}, assignment2.shiftArray(new int[]{}));
+        Assert.assertArrayEquals(new int[]{1}, assignment2.shiftArray(new int[]{1}));
+        Assert.assertArrayEquals(new int[]{2, 3, 1}, assignment2.shiftArray(new int[]{1, 2, 3}));
+        Assert.assertArrayEquals(new int[]{1, 1, 1}, assignment2.shiftArray(new int[]{1, 1, 1}));
+    }
+
+    @Test
+    public void testTripleIncreasingOrder(){
+        Assert.assertEquals(true, assignment2.tripleIncreasingOrder(new int[]{1, 4, 5, 6, 5}));
+        Assert.assertEquals(true, assignment2.tripleIncreasingOrder(new int[]{1, 2, 3}));
+        Assert.assertEquals(false, assignment2.tripleIncreasingOrder(new int[]{1, 2, 4}));
+        Assert.assertEquals(false, assignment2.tripleIncreasingOrder(new int[]{1 }));
+        Assert.assertEquals(true, assignment2.tripleIncreasingOrder(new int[]{1, 2, 4, 5, 7, 6, 5, 6, 7, 6}));
+        Assert.assertEquals(true, assignment2.tripleIncreasingOrder(new int[]{10, 9, 8, -100, -99, -98, 100}));
+    }
+
+    @Test
+    public void testEvenOrOdds() {
+        Assert.assertEquals(false, assignment2.evenOrOdd(new int[]{1, 4, 5, 6, 5}));
+        Assert.assertEquals(false, assignment2.evenOrOdd(new int[]{1, 2, 3}));
+        Assert.assertEquals(true, assignment2.evenOrOdd(new int[]{1, 2, 4, 5}));
+        Assert.assertEquals(true, assignment2.evenOrOdd(new int[]{1, 6}));
+        Assert.assertEquals(true, assignment2.evenOrOdd(new int[]{1, 2, 4, 5, 7, 6, 5, 6, 7, 6}));
+        Assert.assertEquals(false, assignment2.evenOrOdd(new int[]{10, 9, 8, -100, -99, -98, 100}));
+    }
+
+    @AfterClass
+    public static void teardown() {
+        assignment2 = null;
+    }
+
+}
